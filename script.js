1ee751b4dfdea24d51dd974fc4b334940edf5821
maxiCalderonBuono <calderonmaxi@outlook.com>
2021-07-10 15:28:47 -0300

    Nuevos ejercicios

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log (nombre);
-var nombre="Maxi";
+function todosIguales(arreglo) {
+  //Escriba la función todosIguales, que indique si todos los
+
+  let i = 0;
+  while (i < arreglo.length) {
+    if (arreglo[i] !== arreglo[i + 1]) {
+      return false;
+    } else {
+      return true;
+    }
+    i++;
+  }
+}
+
+console.log(todosIguales([1, 1, 1, 1])); //elementos de un arreglo son iguales:
+//retornar true, caso contrario retornar false.
+//Escribe tu código aquí
